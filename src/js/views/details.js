1756e9d29ea000e8d02e854f41837ff1e12f0069
TulioPortela <156851372+Tulio-Portela@users.noreply.github.com>
2024-07-10 16:41:07 +0000

    correcion

--- src/js/views/details.js
+++ src/js/views/details.js
@@ -1,192 +1,149 @@
-import React, { useEffect, useState, useContext } from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useContext } from "react";
+import { Link, useParams, useHistory } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const Details = () => {
-  const [data, setData] = useState([]);
-  const params = useParams();
-  console.log(params);
+const FavoriteIcon = ({ id, type, isFavorite }) => {
+  const { store, actions } = useContext(Context);
 
-  const getData = async () => {
-    const API_URL = "https://www.swapi.tech/api";
-    try {
-      const response = await fetch(`${API_URL}/${params.type}/${params.id}`);
-
-      if (response.ok == true) {
-        const body = await response.json();
-        setData(body.result);
-      } else {
-        const error = response.json();
-        throw new Error(error.message);
-      }
-    } catch (error) {
-      console.log("Error status: ", error);
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      actions.removeFavorite(id);
+    } else {
+      actions.addFavorite(id);
     }
   };
 
+  return (
+    <div onClick={toggleFavorite} style={{ cursor: "pointer" }}>
+      {isFavorite ? (
+        <i className="fas fa-star" style={{ color: "gold" }}></i>
+      ) : (
+        <i className="far fa-star"></i>
+      )}
+    </div>
+  );
+};
+
+export const Details = () => {
+  const { store, actions } = useContext(Context);
+  const params = useParams();
+  const history = useHistory();
+
   useEffect(() => {
-    getData();
+    if (params.type && params.id) {
+      actions.getDetails(params.type, params.id);
+    }
   }, [params.type, params.id]);
+
+  const data = store.details;
+
+  useEffect(() => {
+    if (!data) {
+      history.push("/");
+    }
+  }, [data, history]);
 
   return (
     <React.Fragment>
-      {data.properties ? (
+      {data && data.properties ? (
         <div className="d-flex align-items-center justify-content-center">
-          {params.type == "people" ? (
-            <div className="jumbotron p-4" style={{ width: "65vw" }}>
-              <div className="d-flex align-items-center justify-content-evenly flex-row">
-                <div>
-                  <img
-                    style={{ height: "350px", borderRadius: "10px" }}
-                    src={`https://starwars-visualguide.com/assets/img/characters/${params.id}.jpg`}
+          <div className="jumbotron p-4" style={{ width: "65vw" }}>
+            <div className="d-flex align-items-center justify-content-evenly flex-row">
+              <div>
+                <img
+                  style={{ height: "350px", borderRadius: "10px" }}
+                  src={`https://starwars-visualguide.com/assets/img/${params.type}/${params.id}.jpg`}
+                  alt={data.properties.name}
+                />
+              </div>
+              <div>
+                <h1 className="display-4">
+                  {data.properties.name}{" "}
+                  <FavoriteIcon
+                    id={params.id}
+                    type={params.type}
+                    isFavorite={store.favorites.includes(params.id)}
                   />
-                </div>
-                <div>
-                  <h1 className="display-4">{data.properties.name}</h1>
-                  <p className="lead">{data.description}</p>
-                </div>
-              </div>
-              <hr className="my-4" />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                mollis tempus tortor, quis bibendum tellus luctus vitae. Integer
-                tempor lorem lectus, sed lacinia urna porta ac. In ac arcu
-                cursus, feugiat elit quis, aliquam lacus. Morbi imperdiet
-                finibus tellus vitae feugiat. Fusce lorem enim, commodo eu ex
-                ut, condimentum mattis orci.
-              </p>
-              <div className="d-flex align-items-center justify-content-center flex-row">
-                <p className="bg-light text-dark p-3">
-                  <b>Name:</b> {data.properties.name}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Birth Year:</b> {data.properties.birth_year}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Gender:</b> {data.properties.gender}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Height:</b> {data.properties.height}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Skin Color:</b> {data.properties.skin_color}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Eye Color:</b> {data.properties.eye_color}{" "}
-                </p>
-              </div>
-              <div className="d-flex align-items-center justify-content-center">
-                <Link to="/">
-                  <button type="button" className="btn btn-dark">
-                    Back home!
-                  </button>
-                </Link>
+                </h1>
+                <p className="lead">{data.description}</p>
               </div>
             </div>
-          ) : params.type == "planets" ? (
-            <div className="jumbotron p-4" style={{ width: "65vw" }}>
-              <div className="d-flex align-items-center justify-content-evenly flex-row">
-                <div>
-                  <img
-                    style={{ height: "350px", borderRadius: "10px" }}
-                    src={`https://starwars-visualguide.com/assets/img/planets/${params.id}.jpg`}
-                  />
-                </div>
-                <div>
-                  <h1 className="display-4">{data.properties.name}</h1>
-                  <p className="lead">
-                    {data.description} One of many in the Star Wars universe
+            <hr className="my-4" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              mollis tempus tortor, quis bibendum tellus luctus vitae. Integer
+              tempor lorem lectus, sed lacinia urna porta ac. In ac arcu cursus,
+              feugiat elit quis, aliquam lacus. Morbi imperdiet finibus tellus
+              vitae feugiat. Fusce lorem enim, commodo eu ex ut, condimentum
+              mattis orci.
+            </p>
+            <div className="d-flex align-items-center justify-content-center flex-row">
+              <p className="bg-light text-dark p-3">
+                <b>Name:</b> {data.properties.name}{" "}
+              </p>
+              {params.type === "people" && (
+                <>
+                  <p className="bg-light text-dark p-3">
+                    <b>Birth Year:</b> {data.properties.birth_year}{" "}
                   </p>
-                </div>
-              </div>
-              <hr className="my-4" />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                mollis tempus tortor, quis bibendum tellus luctus vitae. Integer
-                tempor lorem lectus, sed lacinia urna porta ac. In ac arcu
-                cursus, feugiat elit quis, aliquam lacus. Morbi imperdiet
-                finibus tellus vitae feugiat. Fusce lorem enim, commodo eu ex
-                ut, condimentum mattis orci.
-              </p>
-              <div className="d-flex align-items-center justify-content-center flex-row">
-                <p className="bg-light text-dark p-3">
-                  <b>Name:</b> {data.properties.name}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Population:</b> {data.properties.population}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Diameter:</b> {data.properties.diameter}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Climate:</b> {data.properties.climate}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Terrain:</b> {data.properties.terrain}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Gravity:</b> {data.properties.gravity}{" "}
-                </p>
-              </div>
-              <div className="d-flex align-items-center justify-content-center">
-                <Link to="/">
-                  <button type="button" className="btn btn-dark">
-                    Back home!
-                  </button>
-                </Link>
-              </div>
-            </div>
-          ) : (
-            <div className="jumbotron p-4" style={{ width: "65vw" }}>
-              <div className="d-flex align-items-center justify-content-evenly flex-row">
-                <div>
-                  <img
-                    style={{ height: "300px", borderRadius: "10px" }}
-                    src={`https://starwars-visualguide.com/assets/img/vehicles/${params.id}.jpg`}
-                  />
-                </div>
-                <div>
-                  <h1 className="display-4">{data.properties.name}</h1>
-                  <p className="lead">
-                    {data.description}. One of many in the Star Wars universe
+                  <p className="bg-light text-dark p-3">
+                    <b>Gender:</b> {data.properties.gender}{" "}
                   </p>
-                </div>
-              </div>
-              <hr className="my-4" />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                mollis tempus tortor, quis bibendum tellus luctus vitae. Integer
-                tempor lorem lectus, sed lacinia urna porta ac. In ac arcu
-                cursus, feugiat elit quis, aliquam lacus. Morbi imperdiet
-                finibus tellus vitae feugiat. Fusce lorem enim, commodo eu ex
-                ut, condimentum mattis orci.
-              </p>
-              <div className="d-flex align-items-center justify-content-center flex-row">
-                <p className="bg-light text-dark p-3">
-                  <b>Name:</b> {data.properties.name}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Model:</b> {data.properties.model}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Vehicle Class:</b> {data.properties.vehicle_class}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Manufacturer:</b> {data.properties.manufacturer}{" "}
-                </p>
-                <p className="bg-light text-dark p-3">
-                  <b>Cargo Capacity:</b> {data.properties.cargo_capacity}{" "}
-                </p>
-              </div>
-              <div className="d-flex align-items-center justify-content-center">
-                <Link to="/">
-                  <button type="button" className="btn btn-dark">
-                    Back home!
-                  </button>
-                </Link>
-              </div>
+                  <p className="bg-light text-dark p-3">
+                    <b>Height:</b> {data.properties.height}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Skin Color:</b> {data.properties.skin_color}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Eye Color:</b> {data.properties.eye_color}{" "}
+                  </p>
+                </>
+              )}
+              {params.type === "planets" && (
+                <>
+                  <p className="bg-light text-dark p-3">
+                    <b>Population:</b> {data.properties.population}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Diameter:</b> {data.properties.diameter}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Climate:</b> {data.properties.climate}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Terrain:</b> {data.properties.terrain}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Gravity:</b> {data.properties.gravity}{" "}
+                  </p>
+                </>
+              )}
+              {params.type === "vehicles" && (
+                <>
+                  <p className="bg-light text-dark p-3">
+                    <b>Model:</b> {data.properties.model}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Vehicle Class:</b> {data.properties.vehicle_class}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Manufacturer:</b> {data.properties.manufacturer}{" "}
+                  </p>
+                  <p className="bg-light text-dark p-3">
+                    <b>Cargo Capacity:</b> {data.properties.cargo_capacity}{" "}
+                  </p>
+                </>
+              )}
             </div>
-          )}
+            <div className="d-flex align-items-center justify-content-center">
+              <Link to="/">
+                <button type="button" className="btn btn-dark">
+                  Back home!
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       ) : (
         <div
